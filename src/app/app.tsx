e64003a3f8c16a7e56659276cc5ba99ee6fa88a4
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2023-11-09 15:42:04 +0700

    Add react-query client

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import NxWelcome from './nx-welcome';
 
+// Create a client
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <div>
-      <NxWelcome title="awp-project-fe" />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <NxWelcome title="awp-project-fe" />
+      </div>
+    </QueryClientProvider>
   );
 }
 
