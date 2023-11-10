98bf36e5e436fda01d48f0201462f27fee994c2a
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2023-11-10 11:28:31 +0700

    Config react-query and react-router-dom for project

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+function AppLayout() {
+  // TODO: Replace this with your own authentication logic.
+  const isAuthenticated = false;
+
+  const renderBody = () => (
+    <>
+      <div>AppLayout</div>
+      <Outlet />
+    </>
+  );
+
+  return isAuthenticated ? renderBody() : <Navigate to="/sign-in" />;
+}
+
+export default AppLayout;
---
+++ src/routes/index.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from '@/app';
+import ErrorPage from '@/pages/ErrorPage';
+import AppLayout from '@/layouts/AppLayout';
+import Home from '@/pages/HomePage';
+import AuthLayout from '@/layouts/AuthLayout';
+import SignInPage from '@/pages/Auth/SignInPage';
+import LandingPage from '@/pages/LandingPage';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        element: <AppLayout />,
+        children: [
+          {
+            path: '/home',
+            element: <Home />,
+          },
+        ],
+      },
+
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: '/sign-in',
+            element: <SignInPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+export default router;
