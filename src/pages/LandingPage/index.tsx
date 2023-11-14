6c5fec38d4ba52a95fa101a76e7ee1ca03c116ee
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2023-11-14 20:51:55 +0700

    Check auth in Landing Page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,10 +1,20 @@
+import { Navigate } from 'react-router-dom';
+
 import Header from './Header';
 import Hero from './Hero';
 import Features from './Features';
 import TeamMembers from './TeamMembers';
 import Footer from './Footer';
 
+import useAuth from '@/hooks/useAuth';
+
 function LandingPage() {
+  const { user_id } = useAuth();
+
+  if (user_id) {
+    return <Navigate to="/home" />;
+  }
+
   return (
     <div className="twp h-screen max-w-full text-base">
       <Header />
