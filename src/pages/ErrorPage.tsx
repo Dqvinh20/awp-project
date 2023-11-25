4680123824c51ed439b895c8960e7fe3a72a03b0
Thông Vo <thong89x@gmail.com>
2023-11-25 22:39:25 +0700

    Update error pages

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,34 +2,44 @@ import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
 
 import NotFoundPage from './404';
 
-export default function ErrorPage() {
-  const error = useRouteError();
+interface ErrorPageProps {
+  error: Error;
+}
 
-  if (isRouteErrorResponse(error)) {
-    if (error.status === 404) {
-      return <NotFoundPage />;
-    }
+export default function ErrorPage(props: ErrorPageProps) {
+  if (!props.error) {
+    const error = useRouteError();
+    if (isRouteErrorResponse(error)) {
+      if (error.status === 404) {
+        return <NotFoundPage />;
+      }
 
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <div className="text-center flex-row items-center">
-          <h1>Oops!</h1>
-          <p>Sorry, an unexpected error has occurred.</p>
-          <h2 className="twp font-black text-gray-200 text-9xl m-4">
-            {error.status}
-          </h2>
-          <p className="twp mt-4 text-gray-500">{error.statusText}</p>
-          {error.data?.message && <p>{error.data.message}</p>}
-          <a
-            href="/home"
-            className="twp inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
-          >
-            Go Back Home
-          </a>
+      return (
+        <div className="h-screen flex justify-center items-center">
+          <div className="text-center flex-row items-center">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <h2 className="twp font-black text-gray-200 text-9xl m-4">
+              {error.status}
+            </h2>
+            <p className="twp mt-4 text-gray-500">{error.statusText}</p>
+            {error.data?.message && <p>{error.data.message}</p>}
+            <a
+              href="/home"
+              className="twp inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
+            >
+              Go Back Home
+            </a>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 
-  return <div>Sorry, an unexpected error has occurred.</div>;
+  const { error } = props;
+  return (
+    <div className="flex justify-center items-center font-semibold text-lg">
+      Sorry, an unexpected error has occurred. Message: {error.message}
+    </div>
+  );
 }
