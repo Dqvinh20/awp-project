591af211c678f12767195ebba9386badf7e9a4d2
Thông Vo <thong89x@gmail.com>
2023-11-30 02:21:43 +0700

    Thong done Render and Drag Drop grade component

---
+++ src/pages/ClassRoom/component/common/DynamicForm.tsx
@@ -0,0 +1,10 @@
+import { Form, Input } from 'antd'
+import React from 'react'
+
+export default function DynamicForm() {
+  return (
+    <Form.Item label="Diem" name="diem">
+        <Input/>
+    </Form.Item>
+  )
+}
