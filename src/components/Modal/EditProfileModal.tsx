576e469c1e6c040d5e78e9fd277f0f22ca729607
Kwuan <thquan20@clc.fitus.edu.vn>
2023-12-18 13:19:00 +0700

    Add student id for student to update

--- src/components/Modal/EditProfileModal.tsx
+++ src/components/Modal/EditProfileModal.tsx
@@ -7,17 +7,21 @@ import { useQueryClient } from '@tanstack/react-query';
 
 import { useGetMyInfo } from '@/app/store/server/features/users/queries';
 import { useUpdateUser } from '@/app/store/server/features/users/mutations';
+import { USER_ROLE } from '@/app/store/server/features/users/interfaces';
 
 type EditProfileModalProps = Omit<ModalProps, 'onOk'>;
 
 function EditProfileModal(props: EditProfileModalProps) {
   const queryClient = useQueryClient();
-  const { data: myInfo, isLoading } = useGetMyInfo();
+  const { data: myInfo, isLoading, isSuccess } = useGetMyInfo();
   const [form] = Form.useForm();
 
   useEffect(() => {
     if (myInfo) {
       form.setFieldsValue({
+        ...(myInfo.role === USER_ROLE.Student
+          ? { student_id: myInfo.student_id }
+          : {}),
         name: myInfo.full_name,
         first_name: myInfo.first_name,
         last_name: myInfo.last_name,
@@ -58,8 +62,8 @@ function EditProfileModal(props: EditProfileModalProps) {
 
   return (
     <Modal
-      forceRender
       {...props}
+      destroyOnClose
       onOk={() => {
         form.submit();
       }}
@@ -71,73 +75,84 @@ function EditProfileModal(props: EditProfileModalProps) {
           ) : (
             <>
               <h2 className="text-center mb-5">Edit profile</h2>
-              <Form
-                layout="horizontal"
-                onFinish={onFinish}
-                form={form}
-                labelCol={{
-                  span: 4,
-                  offset: 0,
-                }}
-              >
-                <Form.Item
-                  label="Name"
-                  style={{ marginBottom: 0 }}
-                  wrapperCol={{
-                    span: 20,
+              {isSuccess && (
+                <Form
+                  layout="horizontal"
+                  onFinish={onFinish}
+                  form={form}
+                  labelCol={{
+                    span: 6,
+                    offset: 0,
                   }}
                 >
-                  <Form.Item
-                    name="first_name"
-                    style={{
-                      display: 'inline-block',
-                      width: 'calc(50% - 8px)',
-                    }}
-                    rules={[
-                      { required: true, message: 'Please input first name!' },
-                    ]}
-                  >
-                    <Input
-                      placeholder="Input First name"
-                      value={myInfo?.first_name}
-                    />
+                  <Form.Item name="email" label="Email">
+                    <Input disabled defaultValue={myInfo.email} />
                   </Form.Item>
+
                   <Form.Item
-                    name="last_name"
-                    rules={[{ required: true, message: 'Please last name!' }]}
-                    style={{
-                      display: 'inline-block',
-                      width: 'calc(50% - 0px)',
-                      margin: '0 0 0 8px',
+                    label="Name"
+                    style={{ marginBottom: 0 }}
+                    wrapperCol={{
+                      span: 20,
                     }}
                   >
-                    <Input
-                      placeholder="Input last name"
-                      value={myInfo?.last_name}
-                    />
+                    <Form.Item
+                      name="first_name"
+                      style={{
+                        display: 'inline-block',
+                        width: 'calc(50% - 8px)',
+                      }}
+                      rules={[
+                        { required: true, message: 'Please input first name!' },
+                      ]}
+                    >
+                      <Input
+                        placeholder="Input First name"
+                        value={myInfo?.first_name}
+                      />
+                    </Form.Item>
+                    <Form.Item
+                      name="last_name"
+                      rules={[{ required: true, message: 'Please last name!' }]}
+                      style={{
+                        display: 'inline-block',
+                        width: 'calc(50% - 0px)',
+                        margin: '0 0 0 8px',
+                      }}
+                    >
+                      <Input
+                        placeholder="Input last name"
+                        value={myInfo?.last_name}
+                      />
+                    </Form.Item>
                   </Form.Item>
-                </Form.Item>
-                <Form.Item name="gender" label="Gender">
-                  <Select placeholder="Select your gender">
-                    <Select.Option value="Male">Male</Select.Option>
-                    <Select.Option value="Female">Female</Select.Option>
-                    <Select.Option value="Other">Other</Select.Option>
-                  </Select>
-                </Form.Item>
-                {/* <Form.Item
-                name="Address"
-                label="Address"
-                rules={[
-                  {
-                    type: 'array',
-                    required: true,
-                    message: 'Please select your address!',
-                  },
-                ]}
-              >
-                <Cascader placeholder="Chose address" options={residences} />
-              </Form.Item> */}
-              </Form>
+                  {myInfo?.role === USER_ROLE.Student && (
+                    <Form.Item
+                      name="student_id"
+                      label="Student ID"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Student ID is required',
+                        },
+                        {
+                          min: 0,
+                          max: 10,
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Update student ID" />
+                    </Form.Item>
+                  )}
+                  <Form.Item name="gender" label="Gender">
+                    <Select placeholder="Select your gender">
+                      <Select.Option value="Male">Male</Select.Option>
+                      <Select.Option value="Female">Female</Select.Option>
+                      <Select.Option value="Other">Other</Select.Option>
+                    </Select>
+                  </Form.Item>
+                </Form>
+              )}
             </>
           )}
         </div>
