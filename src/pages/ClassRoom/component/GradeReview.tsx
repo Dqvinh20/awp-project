185f632731e68849b47c4c0b53adcc5cd05cce30
Kwuan <thquan20@clc.fitus.edu.vn>
2023-12-18 11:19:42 +0700

    Fix GradeReview Page

--- src/pages/ClassRoom/component/GradeReview.tsx
+++ src/pages/ClassRoom/component/GradeReview.tsx
@@ -1,5 +1,3 @@
-import { useParams } from 'react-router-dom';
-
 import { Space, Spin } from 'antd';
 
 import CreateRequestGradeReview from './button/CreateRequestGradeReview';
@@ -7,28 +5,27 @@ import StudentGradeReviewItem from './GradeReviewItem/StudentGradeReviewItem';
 
 import TeacherGradeReviewItem from './GradeReviewItem/TeacherGradeReviewItem';
 
-import { useClassGradeReview } from '@/app/store/server/features/grade_review/queries';
 import { useUserRole } from '@/hooks/useUserRole';
 import { USER_ROLE } from '@/app/store/server/features/users/interfaces';
 import {
   StudentGradeReviewDto,
   TeacherGradeReviewDto,
 } from '@/app/store/server/features/grade_review/interfaces';
+import useClassGradeReview from '@/hooks/useClassGradeReview';
 
 function GradeReview() {
-  const { id: classId } = useParams<{ id: string }>();
   const {
     data,
     isSuccess: gradeReviewSuccess,
     isLoading: isGradeReviewLoading,
-  } = useClassGradeReview(classId);
+  } = useClassGradeReview();
   const hasData = data?.length > 0;
 
   const userRole = useUserRole();
 
   return (
-    <div className="h-screen-dvh w-full p-4  mb-52">
-      <CreateRequestGradeReview />
+    <div className="h-full w-full p-4">
+      {userRole === USER_ROLE.Student && <CreateRequestGradeReview />}
       {isGradeReviewLoading && (
         <div className="flex justify-center items-center h-full">
           <Spin />
